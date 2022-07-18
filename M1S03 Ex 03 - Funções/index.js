51bfacc26f2467e3b9eed921009f0030d7e7f1f2
michaelrlas <michaelsilvamkt@gmail.com>
2022-07-18 18:58:35 -0300

    funções e dom

--- M1S03 Ex 03 - Funções/index.js
+++ M1S03 Ex 03 - Funções/index.js
@@ -1,21 +1,32 @@
-var lista = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var listaTamanho = lista.length;
-var contadorPar = 0;
-var contadorImpar = 0;
-function ContadorParImpar(lista) {
-  if (lista % 2 == 0) {
-    contadorPar++;
-  } else if (!(lista % 2 != 1)) {
-    contadorImpar++;
+var listaDeNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// var listaDeLetras = ['oi', 'turu', 'bem'];
+function ContadorParImpar(lista = []) {
+  var listaTamanho = lista.length;
+  var contadorPar = 0;
+  var contadorImpar = 0;
+  var invalido;
+  for (let i = 0; i < lista.length; i++) {
+    if (lista[i] % 2 == 0) {
+      contadorPar++;
+    } else if (lista[i] % 2 == 1) {
+      contadorImpar++;
+    } else {
+      invalido++;
+    }
   }
+
+  return [contadorPar, contadorImpar, listaTamanho];
 }
 
-lista.forEach(ContadorParImpar);
+var par = ContadorParImpar(listaDeNumeros)[0];
+var impar = ContadorParImpar(listaDeNumeros)[1];
+var listaQtt = ContadorParImpar(listaDeNumeros)[2];
+
 console.log(
   'A quantidade informada foi',
-  listaTamanho,
+  listaQtt,
   'a de valores pares foi ',
-  contadorPar,
+  par,
   ', e a de valores ímpares foi ',
-  contadorImpar
+  impar
 );
---
+++ M1S03 Ex 04 - DOM/index.js
@@ -0,0 +1,13 @@
+var divTexto;
+const texto1 = 'Texto 1;',
+  texto2 = ' Texto 2;',
+  texto3 = ' Texto 3;',
+  texto4 = ' Texto 4;',
+  texto5 = ' Texto5 ;';
+
+minhaDiv = document.getElementById('div-texto');
+minhaDiv.innerHTML += texto1;
+minhaDiv.innerHTML += texto2;
+minhaDiv.innerHTML += texto3;
+minhaDiv.innerHTML += texto4;
+minhaDiv.innerHTML += texto5;
